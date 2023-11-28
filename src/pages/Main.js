3873ac5ee43b318e957a61e4b8974ff1aabbbe4c
happyjino <wlsgh1128@naver.com>
2023-11-28 12:30:28 +0900

    메인페이지 일부 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -49,41 +49,40 @@ const Main = () => {
       <div className="section2">
         <div>
           <p className="tel-box">
-            {`꽃피는 고래에 대해서 알고 싶으시다면? \n010-1234-5678`}
+            {`꽃피는 고래에 대해서 알고 싶으시다면? \n052-244-0228`}
           </p>
           <p className="text-box">
             꽃피는 고래를 소개합니다.
           </p>
           <div className="cate-link">
             <div>
-              <div className="link-button">FARMER</div>
+              <div className="link-button">COMPANY</div>
               <ul>
-                <li>Smart farm</li>
-                <li>청년 농부</li>
-                <li>보령</li>
+                <li>회사 의미</li>
+                <li>꽃피는 고래</li>
+                <li>회사 연혁</li>
               </ul>
             </div>
             <div>
-              <div className="link-button">CUCUMBER</div>
+              <div className="link-button">SYSTEM</div>
               <ul>
-                <li>미니오이</li>
-                <li>백다다기</li>
-                <li>취청</li>
+                <li>그린하우스 재배</li>
+                <li>친환경 재배 농법</li>
+                <li>순환식 분무수경재배 방식</li>
               </ul>
             </div>
             <div>
-              <div className="link-button">SERVICES</div>
+              <div className="link-button">VEGETABLE</div>
               <ul>
-                <li>스마트팜 농장 체험/교육</li>
-                <li>스마트 농부 아카데미</li>
+                <li>채소 종류</li>
+                <li>활용 방안</li>
               </ul>
             </div>
             <div>
-              <div className="link-button">PRODUCTS</div>
+              <div className="link-button">PRODUCT</div>
               <ul>
-                <li>GREEN MONSTERS</li>
-                <li>캐릭터 상품</li>
-                <li>오이 상품</li>
+                <li>기본 상품</li>
+                <li>구독 상품</li>
               </ul>
             </div>
           </div>
@@ -93,7 +92,7 @@ const Main = () => {
         <span class="material-symbols-outlined">arrow_back_ios</span>
         <div className="text-box">
           <h2>안녕하세요. 꽃피는고래 입니다!</h2>
-          <p>{`​그린몬스터즈는 스마트팜 보육 사업 1기 수료생들과 농부의 꿈을 가진 청년들이 모여 최초로 사업화에 성공해 만든 스마트팜 오이 농장입니다! 스마트팜을 배경으로 농업 분야에서 괴물 같은 농부들이 되기 위해 각 분야의 역량이 뛰어난 청년들이 모였습니다.`}</p>
+          <p>{`저희 농업회사법인 주식회사 꽃피는 고래는 발달장애인과 함께 수경재배방식으로 유러피안 채소를 재배하는 농원입니다. 자연까지 생각하는 기업, 꽃피는 그린하우스에서 정성으로 키워내 여러분의 문 앞에 건강함과 신선함을 전하겠습니다.`}</p>
         </div>
         <span class="material-symbols-outlined">arrow_forward_ios</span>
       </div>
